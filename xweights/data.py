b8ec58901cbba36b9b71cfcef31eb270cf3c9909
Ludwig.Lierhammer <ludwig.lierhammer@hzg.de>
2022-03-09 17:06:45 +0100

    Updated

--- xweights/data.py
+++ xweights/data.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 data_path = Path(__file__).parent 
 
-nclist = list((data_path / "data/netcdf").glob('*').as_posix())
+nclist = list((data_path / "data/netcdf").glob('*')
 netcdf = [nc.as_posix() for nc in nclist]
 
 shplist = list((data_path / "data/shp").glob('*.shp'))
