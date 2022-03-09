0477e88adbbfd6da0263744490a1e6ae950b66ce
Ludwig Lierhammer <ludwig.lierhammer@hereon.de>
2022-03-09 14:22:41 +0100

    Update test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,6 +7,6 @@ from . import has_intake, requires_intake
 from . import has_dask, requires_dask
 
 def test_get_dataset_dict():
-    netcdffile = '../test/data/netcdf/tas_EUR-11_MIROC-MIROC5_rcp85_r1i1p1_CLMcom-CCLM4-8-17_v1_mon_200701-200712.nc'
+    netcdffile = '../tests/data/netcdf/tas_EUR-11_MIROC-MIROC5_rcp85_r1i1p1_CLMcom-CCLM4-8-17_v1_mon_200701-200712.nc'
     
     assert xw.Input(netcdffile).dataset_dict
