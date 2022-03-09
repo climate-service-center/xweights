e17c7a1de80f080291c7dc392beaa1b0420773f9
Ludwig Lierhammer <ludwig.lierhammer@hereon.de>
2022-03-09 14:23:26 +0100

    Update test_xesmf.py

--- tests/test_xesmf.py
+++ tests/test_xesmf.py
@@ -9,7 +9,7 @@ from . import has_xesmf, requires_xesmf
 from . import has_numpy, requires_numpy
 
 def test_spatial_averager():
-    netcdffile = './data/netcdf/tas_EUR-11_MIROC-MIROC5_rcp85_r1i1p1_CLMcom-CCLM4-8-17_v1_mon_200701-200712.nc'
+    netcdffile = '../tests/data/netcdf/tas_EUR-11_MIROC-MIROC5_rcp85_r1i1p1_CLMcom-CCLM4-8-17_v1_mon_200701-200712.nc'
 
     shpfile = xw.get_region('states')
     ds = xr.open_dataset(netcdffile)
