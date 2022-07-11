7dd08b57c956a2a8885787f0d8db3e6412e202d0
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-07-11 14:07:44 +0200

    tests for python3.8 to python3.10

--- setup.py
+++ setup.py
@@ -39,9 +39,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Python xweights contains functions"
     "to create grid weighted area means",
