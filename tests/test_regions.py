ab85e9d94f1fa0e18e7813631ebb6d813169931a
Katharina Bülow <g300047@levante0.lvt.dkrz.de>
2023-09-14 13:49:40 +0200

    changed test

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -13,17 +13,21 @@ def test_which_regions():
 
 
 def test_which_subregions():
-    assert xw.which_subregions("states")
     assert xw.which_subregions("counties")
+    assert xw.which_subregions("counties_merged")
+    assert xw.which_subregions("ipcc")
     assert xw.which_subregions("prudence")
+    assert xw.which_subregions("states")
 
 
 def test_get_region():
+    xw.get_region("counties")
+    xw.get_region("counties_merged")
+    xw.get_region("ipcc")
+    xw.get_region("prudence")
     xw.get_region("states")
     xw.get_region("states", name="02_Hamburg")
     xw.get_region("states", name=["01_Schleswig-Holstein", "02_Hamburg"], merge="all")
-    xw.get_region("counties")
-    xw.get_region("prudence")
 
 
 def test_get_user_region():
